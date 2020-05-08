d4db80076c017a441b9f2469c1d58c59bcc0d0e1
Lucamsat <64135400+Lucamsat@users.noreply.github.com>
2020-05-08 14:39:46 +0200

    Update BCreader_Publisher.py

--- onRB/BCreader_Publisher.py
+++ onRB/BCreader_Publisher.py
@@ -1,8 +1,10 @@
+# Sensor BC publisher which reads the data from the sensor and outputs a json message to the broker using the topic "user/barcode"
+
+
 from BCreader import BC_Reader
 import paho.mqtt.client as mqttc
 import time
 import datetime
-#import requests
 import json
 
 class PublishData(object): 
@@ -24,7 +26,6 @@ class PublishData(object):
 # acknowledgement is sent that results in the on_publish callback being called.
     @classmethod # Create Function for Callback
     def on_publish(cls, client, userdata, mid):
-        # cls is equal to self but for class methods 
         # get the current time
         get_time = datetime.datetime.now()
         current_time =  get_time.strftime("%Y-%m-%d %H:%M:%S")
@@ -38,25 +39,19 @@ class PublishData(object):
         #This function will publish the data related to sensor BC 
         while 4==4:    
             try:
+                # get data from the sensor in a json
                 inputJsonFromBCSensor = self.data.senseBC()
                 inputData = json.loads(inputJsonFromBCSensor)
+                # manipulate the json
                 BC = inputData["BC"]
                 time1 = inputData["time"]
+                # get all information required for the exchange
                 outputJson=json.dumps({"subject":"BARCODE_sensor_data", "BC": BC, "time":time1})
+                # publish the message 
                 client.publish("user/barcode", str(outputJson), qos=1)
-    #            msg_info = client.publish("user/ID_fiscal_code", str(outputJson), qos=1)
-    #            if msg_info.is_published() == True:
-    #                print ("\n ID_Publisher : Message is published.")
-                # This call will block until the message is published
-                #msg_info.wait_for_publish()
-                #return ("HELLO") # <-- andava dentro json_format
                 print("MOVE THE CAMERA FROM THE BARCODE") 
                 time.sleep(10)                
             except:
-                #get_time = datetime.datetime.now()
-                #current_time = get_time.strftime("%Y-%m-%d %H:%M:%S")
-                #print("BC_Reader_sensor: ERROR IN PUBLISHING DATA RELATED TO THE SENSORS")
-                #print ("at time: " + str(current_time))
                 print("PLEASE SCAN A BARCODE") 
                 time.sleep(5)
 
@@ -65,12 +60,13 @@ class PublishData(object):
 if __name__ == '__main__': 
     
     try:
-        # create an object from ID_Reader class
+        # create an object from BC_Reader class
         sensor_data = BC_Reader()
     except:
         print("BC_Reader_sensor: ERROR IN GETTING DATA FROM SENSOR ")
     
     try: 
+        # get all information of the config file
         file = open("configFile.json", 'r')
         json_string = file.read()    
         file.close()
@@ -78,20 +74,20 @@ if __name__ == '__main__':
         raise KeyError("ID_Reader_Publisher: ERROR IN READING FILE ")
     
 
+    # get the json of the file information 
     data = json.loads(json_string)
     broker_address = data["resourceCatalog"]["broker_address"]
     port1 = data["resourceCatalog"]["port1"]
     port2 = data["resourceCatalog"]["port2"]
 
-    "creo un client"
+    "create a client"
     client = mqttc.Client()
-
+    # create a publish object
     bar = PublishData(sensor_data, client)
 
 
     try:
-        "ti permette di attaccare le callback al client che in questo caso si comporta da publisher"
-        " assegna delle funzioni alla callback "
+        "attachment of callbacks to client "
         client.on_connect = PublishData.on_connect
         client.on_publish = PublishData.on_publish
         client.connect(broker_address, int(port1), int(port2))
